18e2785bf64b172f3f077a811617a178ddb484f8
c.huang <chuang@netprotections.co.jp>
2024-02-01 20:30:54 +0800

    feat: test structure and test code with HeroController

--- src/tests/controllers/HeroController.test.ts
+++ src/tests/controllers/HeroController.test.ts
@@ -62,24 +62,25 @@ describe('HeroController', () => {
         })
     })
 
+
+    const setAuthenticationMock = (result: string) => {
+        const mockComparePassword = jest.fn().mockResolvedValue(result);
+        AuthenticatePersistence.comparePassword = mockComparePassword;
+    };
+
+    const mockGetHerosAndProfiles = (data: Array<Object>) => {
+        const mockGetHerosAndProfiles = jest.fn().mockResolvedValue(data);
+        HeroPersistence.getHerosAndProfiles = mockGetHerosAndProfiles
+    }
+
     describe("#getAuthenticatedHeros", () => {
-
-        const setAuthenticationMock = (result: string) => {
-            const mockComparePassword = jest.fn().mockResolvedValue(result);
-            AuthenticatePersistence.comparePassword = mockComparePassword;
-        };
-
-        const mockGetHerosAndProfiles = (data: Object) => {
-            const mockGetHerosAndProfiles = jest.fn().mockResolvedValue(data);
-            HeroPersistence.getHerosAndProfiles = mockGetHerosAndProfiles
-        }
 
         describe("Context1: password is correct", () => {
             beforeEach(() => {
                 setAuthenticationMock("succeed")
                 mockGetHerosAndProfiles([
                     { id_from_external: 1, name: "fake_h_1", image: "fake_i_1", str: 2, int: 7, agi: 9, luk: 7 } ])})
-            it("will get hero and profile", async () => {
+            it("will get heroes and profiles", async () => {
                 const result = await HeroController.getAuthenticatedHeros("fake_name", "fake_password")
 
                 expect(result).toEqual({ 
@@ -101,10 +102,73 @@ describe('HeroController', () => {
 
         })
 
-        // describe("Context2: password is NOT correct", async () => {
+        describe("Context2: password is NOT correct", () => {
+            beforeEach(() => {
+                setAuthenticationMock("no username")
+                getHerosMock([{ id: "1", name: "fake_h_1", image: "fake_i_1" }])
+            })
+            it("will get hero only", async () => {
+                const result = await HeroController.getAuthenticatedHeros("fake_name", "fake_password")
+                
+                expect(result).toEqual( { heroes: [{ id: "1", name: "fake_h_1", image: "fake_i_1" }] })
+                expect(HeroPersistence.getHeros).toHaveBeenCalledTimes(1)
+                expect(AuthenticatePersistence.comparePassword).toHaveBeenCalledWith("fake_name", "fake_password")
+
+            })
             
-        // })
+        })
         
+    })
+
+    const mockGetHeroAndProfile = (data: Object) => {
+        const mockGetHeroAndProfile = jest.fn().mockResolvedValue(data)
+        HeroPersistence.getHeroAndProfile = mockGetHeroAndProfile
+
+    }
+    describe("#getAuthenticatedHero", () => {
+        describe("Context 1: password is correct", () => {
+            beforeEach(() => {
+                setAuthenticationMock("succeed")
+                mockGetHeroAndProfile({ id_from_external: 1, name: "fake_h_1", image: "fake_i_1", str: 2, int: 7, agi: 9, luk: 7 })
+            })
+            it("will get hero and profile", async () => {
+                const result = await HeroController.getAuthenticatedHero("fake_user", "fake_password", "1")
+    
+                expect(result).toEqual({ id: "1", 
+                            name: "fake_h_1", 
+                            image: "fake_i_1", 
+                            profile: {
+                                "str": 2,
+                                "int": 7,
+                                "agi": 9,
+                                "luk": 7 }
+                            })
+
+                expect(AuthenticatePersistence.comparePassword).toHaveBeenCalledWith("fake_user", "fake_password")
+                expect(HeroPersistence.getHeroAndProfile).toHaveBeenCalledWith("1")
+            })
+        })
+        
+
+
+        describe("Context 2: password is NOT correct", () => {
+            beforeEach(() => {
+                setAuthenticationMock("Password is wrong")
+                getHeromock({ id: "1", name: "fake_h_1", image: "fake_i_1"})
+            })
+            it("will get hero and profile", async () => {
+                const result = await HeroController.getAuthenticatedHero("fake_user", "fake_password", "1")
+    
+                expect(result).toEqual({ id: "1", 
+                            name: "fake_h_1", 
+                            image: "fake_i_1"})
+
+                expect(AuthenticatePersistence.comparePassword).toHaveBeenCalledWith("fake_user", "fake_password")
+                expect(HeroPersistence.getHero).toHaveBeenCalledWith(1)
+    
+            })
+        })
+
     })
 
 })
