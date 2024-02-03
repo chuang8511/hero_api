b994f59a102ba90560735057465b6740412c5424
c.huang <chuang@netprotections.co.jp>
2024-02-03 16:56:45 +0800

    feat: add the single hero api connection for the future possible usage

--- src/apis/apiUrls.ts
+++ src/apis/apiUrls.ts
@@ -1,2 +1,3 @@
 export const HEROES_API_URL = 'https://hahow-recruit.herokuapp.com/heroes';
-export const HERO_PROFILE_API_URL = (heroId: number) => `https://hahow-recruit.herokuapp.com/heroes/${heroId}/profile`;
+export const SINGEL_HEROES_API_URL = (heroId: string) => `https://hahow-recruit.herokuapp.com/heroes/${heroId}`
+export const HERO_PROFILE_API_URL = (heroId: string) => `https://hahow-recruit.herokuapp.com/heroes/${heroId}/profile`;
